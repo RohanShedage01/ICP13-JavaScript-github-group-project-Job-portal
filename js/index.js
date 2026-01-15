293311aeaad7dd83f0d47357a07a7dfcc20cac04
Abhisanap25 <abhisheksanap107@gmail.com>
2026-01-15 11:03:59 +0530

    added js and css pages

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+function togglePassword() {
+        const pwd = document.getElementById("password");
+        const toggleText = document.querySelector(".toggle");
+
+        if (pwd.type === "password") {
+            pwd.type = "text";
+            toggleText.innerText = "😊";
+        } else {
+            pwd.type = "password";
+            toggleText.innerText = "👀";
+            
+        }
+    }
